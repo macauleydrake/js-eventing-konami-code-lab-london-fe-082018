d306e84c9c8ab0e1e7861abacbc1b2ccd3ddea4b
Macauley Drake <macauleyann@gmail.com>
2018-10-03 19:04:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   document.body.addEventListener('keydown', function(event) {
   alert(event.which)
-    
   })
+  if
 }
+
 
 init();
